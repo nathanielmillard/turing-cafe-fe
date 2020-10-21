141bc978895ffeccdfc31a4a863d65ad16490755
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-21 11:16:23 -0600

    Add functionality to add new reservations.

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from '../Card/Card'
+import Form from '../Form/Form'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       reservations: [],
-
     }
   }
-  getAllReservations() {
+  getAllReservations = () => {
+    if(!this.state.reservations.length){
     fetch('http://localhost:3001/api/v1/reservations')
     .then( response => {
       if(response.ok){
@@ -25,15 +26,23 @@ class App extends Component {
     )
     .catch(error => {
       console.log(error)
-    })
+    })}
   }
 
-  displayReservations() {
+  makeReservation = (reservation) => {
+    let allReservations = this.state.reservations
+    allReservations.push(reservation)
+    this.setState({reservations: allReservations})
+    // this.displayReservations();
+  }
+
+  displayReservations = () => {
     if (!this.state.reservations.length){
       return <h1>Loading your reservations...</h1>
     } else {
       let reservationCards = this.state.reservations.map(reservation => {
         return (<Card
+          key={reservation.id}
           name={reservation.name}
           date={reservation.date}
           time={reservation.time}
@@ -46,12 +55,13 @@ class App extends Component {
 
   render() {
     this.getAllReservations()
-
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+        <Form
+          makeReservation={this.makeReservation}
+        />
         </div>
         <div className='resy-container'>
           {this.displayReservations()}
