7cb67d8a8eb4e80063c5f466b843b677fda06c1c
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-21 10:07:11 -0600

    Get and display reservations from the api.

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from '../Card/Card'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reservations: [],
+
+    }
+  }
+  getAllReservations() {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then( response => {
+      if(response.ok){
+        return response.json()
+      } else {
+        throw Error
+      }
+    })
+    .then( response => {
+      this.setState({reservations: response})
+    }
+    )
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  displayReservations() {
+    if (!this.state.reservations.length){
+      return <h1>Loading your reservations...</h1>
+    } else {
+      let reservationCards = this.state.reservations.map(reservation => {
+        return (<Card
+          name={reservation.name}
+          date={reservation.date}
+          time={reservation.time}
+          number={reservation.number}
+          />)
+      })
+      return reservationCards
+    }
+  }
+
   render() {
+    this.getAllReservations()
+
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
@@ -10,7 +54,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          {this.displayReservations()}
         </div>
       </div>
     )
